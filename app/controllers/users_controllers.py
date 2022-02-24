870a323f8719919e82b1396e3efd316f60bc1006
wallacesfos <wallace.oliveira@aluno.faculdadeimpacta.com.br>
2022-02-24 10:07:44 -0300

    Feat: Finish Route Users

--- app/controllers/users_controllers.py
+++ app/controllers/users_controllers.py
@@ -1,6 +1,9 @@
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from app.exc import PermissionError
 from app.models.user_model import UserModel
 from flask import request, current_app
+from sqlalchemy import exc
+
 from app import utils
 
 
@@ -9,12 +12,12 @@ def update_users():
     body = request.get_json()
     
     try:
-        utils.analyze_keys(["password"])
+        utils.analyze_keys(["password"], body)
         requests = get_jwt_identity()
         found_user = UserModel.query.filter_by(email=requests['email']).first()
 
         for key, value in body.items():
-            found_user.password = value
+            found_user.password_to_hash = value
 
         current_app.db.session.add(found_user)
         current_app.db.session.commit()
@@ -24,3 +27,52 @@ def update_users():
         return {"error": str(e)}, 400
     except Exception:
         return {"error": "An unexpected error occurred"}, 400
+
+
+@jwt_required()
+def delete_user():
+    user = get_jwt_identity()
+    found_user = UserModel.query.filter_by(email=user['email']).first()
+    current_app.db.session.delete(found_user)
+    current_app.db.session.commit()
+
+    return {}, 204
+
+
+@jwt_required()
+def update_users_admin():
+    body = request.get_json()
+    requests = get_jwt_identity()
+    
+    try:
+        if requests['administer'] == False:
+            raise PermissionError('Permission denied')
+
+        found_user = UserModel.query.filter_by(id=body['id']).first()
+
+        if not found_user:
+            return {"message": "User not found"}, 404
+
+        for key, value in body.items():
+            if key == 'password':
+                found_user.password_to_hash = value
+            else:
+                setattr(found_user, key, value)
+
+        current_app.db.session.add(found_user)
+        current_app.db.session.commit()
+        
+        return {
+            "id": found_user.id,
+            "email": found_user.email
+        }
+    
+    except PermissionError as e:
+        return {"error": str(e)}, 400
+    except KeyError:
+        return {"error": "Need to pass the ID"}, 400
+    except exc.StatementError:
+        return {"error": "id: integer, email: string, administer: Boolean"}
+    except Exception:
+        return {"error": "An unexpected error occurred"}, 400
+
--- app/routes/users_route.py
+++ app/routes/users_route.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
+from app.controllers.users_controllers import update_users, delete_user, update_users_admin
 
 bp_users = Blueprint("users", __name__, url_prefix="/users")
 
-bp_users.post('')()
+bp_users.put('')(update_users)
+bp_users.delete('')(delete_user)
+bp_users.put('/to_admin')(update_users_admin)
