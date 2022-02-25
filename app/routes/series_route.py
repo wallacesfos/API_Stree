53f5e4da8f49fa24d0dbd21a3d927a50d8a8943d
juliagama <gamaj235@gmail.com>
2022-02-24 23:22:49 -0300

    feat:finished get series

--- app/routes/series_route.py
+++ app/routes/series_route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from app.controllers.series_controller import create_serie
+from app.controllers.series_controller import create_serie,get_series
 
 bp_series = Blueprint("series", __name__, url_prefix="/series")
 
 bp_series.post("")(create_serie)
+bp_series.get("")(get_series)
