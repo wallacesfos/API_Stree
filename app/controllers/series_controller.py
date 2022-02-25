36492a2e049b0089e1ea8c2a974c02ea1ffcb5a6
juliagama <gamaj235@gmail.com>
2022-02-25 14:21:51 -0300

    feat:finished get-series-by-name

--- app/controllers/series_controller.py
+++ app/controllers/series_controller.py
@@ -12,6 +12,7 @@ def create_serie():
         keys = ["name", "image", "description", "seasons", "subtitle", "dubbed", "trailer", "classification", "released_date"]
         
         administer = get_jwt_identity()
+        
 
         if not administer["administer"]:
             raise PermissionError
@@ -53,3 +54,24 @@ def get_serie_by_id(id):
         return {"message": "Serie not found"}, 404
 
     return jsonify(serie),200
+
+@jwt_required()
+def get_serie_by_name():
+    serie_name = request.args.get("name")
+    serie_name = serie_name.title()
+    new_str = ""
+
+    for i in serie_name:
+        if i == "%":
+            new_str += " "
+        else:
+            new_str += i
+            
+    
+    serie = SeriesModel.query.filter_by(name=new_str).first()
+
+    if not serie:
+        return {"message": "Serie not found"}, 404
+
+    return jsonify(serie),200
+    
