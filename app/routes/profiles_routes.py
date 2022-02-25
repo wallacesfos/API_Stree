5f5d80ebc3496dbbbb810b2e9712287682a9e9b3
wallacesfos <wallace.oliveira@aluno.faculdadeimpacta.com.br>
2022-02-25 13:47:21 -0300

    feat: Routes Patch profile and max profiles add

--- app/routes/profiles_routes.py
+++ app/routes/profiles_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 from app.models.profile_model import ProfileModel
-from app.controllers.profiles_controller import create_profile, get_profiles
+from app.controllers.profiles_controller import create_profile, get_profiles, update_profile
 
 bp_profile = Blueprint('profile', __name__, url_prefix="/profiles")
 
 bp_profile.post('')(create_profile)
 bp_profile.get('')(get_profiles)
+bp_profile.patch('/<int:id>')(update_profile)
