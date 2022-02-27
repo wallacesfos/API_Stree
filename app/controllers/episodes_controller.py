4ce321a41d3fe043cf31046829a5821174da5665
juliagama <gamaj235@gmail.com>
2022-02-27 01:58:38 -0300

    feat:finished post-episodes

---
+++ app/controllers/episodes_controller.py
@@ -0,0 +1,38 @@
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask import request, current_app, jsonify
+from app.models.episodes_model import EpisodesModel
+from app.models.series_model import SeriesModel
+from app.utils import analyze_keys
+from app.exc import PermissionError
+
+@jwt_required()
+def create_episode():
+    try:
+        session = current_app.db.session
+        data = request.get_json()
+        keys = ["season", "link", "series_id", "episode"]
+        
+        administer = get_jwt_identity()
+        
+
+        if not administer["administer"]:
+            raise PermissionError
+
+        analyze_keys(keys, data)
+
+        episode = EpisodesModel(**data)
+
+        session.add(episode)
+        session.commit()
+
+        return jsonify(episode), 201
+
+    except PermissionError:
+        return {"error": "Admins only"},400
+
+    except KeyError as e:
+        return {"error": str(e)}
+
+    except Exception:
+        return {"error": "An unexpected error occurred"}, 400
+
