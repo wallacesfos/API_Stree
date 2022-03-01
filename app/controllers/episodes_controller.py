e1a1501d85d2a2c7fe5974346196b97336712ea3
juliagama <gamaj235@gmail.com>
2022-03-01 20:50:56 -0300

    fix:add except not found serie_id

--- app/controllers/episodes_controller.py
+++ app/controllers/episodes_controller.py
@@ -1,10 +1,10 @@
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask import request, current_app, jsonify
+from werkzeug.exceptions import NotFound
 from app.models.episodes_model import EpisodesModel
 from app.models.series_model import SeriesModel
 from app.utils import analyze_keys
 from app.exc import PermissionError
-
 
 @jwt_required()
 def get_episodes():
@@ -17,28 +17,29 @@ def get_episodes():
 
 @jwt_required()
 def get_episode_by_serie_id(serie_id):
-    serie = SeriesModel.query.filter_by(id=serie_id).first()
-    episode = EpisodesModel.query.filter_by(id=serie_id).first()
+    try:
+        serie = SeriesModel.query.filter_by(id=serie_id).first_or_404(description="Serie id not found!")
+        episode = EpisodesModel.query.filter_by(id=serie_id).first_or_404(description="Serie id not found!")
 
-    episode_serialize = {
-        
-        "series_id": episode.series_id,
-        "name": serie.name,
-        "image": serie.image,
-        "description": serie.description,
-        "subtitle": serie.subtitle,
-        "dubled": serie.dubbed,
-        "episodes":[
-            {
-                "season": episode.season, 
-                "link": episode.link, 
-                "series_id": episode.series_id,
-                "episode": episode.episode
-            }
-        ]
-    }
-    
-    if not serie:
-        return {"message": "Episode not found"}, 404
+        episode_serialize = {
+            
+            "series_id": episode.series_id,
+            "name": serie.name,
+            "image": serie.image,
+            "description": serie.description,
+            "subtitle": serie.subtitle,
+            "dubled": serie.dubbed,
+            "episodes":[
+                {
+                    "season": episode.season, 
+                    "link": episode.link, 
+                    "series_id": episode.series_id,
+                    "episode": episode.episode
+                }
+            ]
+        }
 
-    return jsonify(episode_serialize),200
+        return jsonify(episode_serialize),200
+
+    except NotFound as e:
+        return {"error": e.description}, 404
