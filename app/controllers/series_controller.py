8fe31a4566ece9e6f5314f3050158c2e1900f33e
IqueMoraes <iquedemoraes@hotmail.com>
2022-03-02 14:25:58 -0300

    fix series controller

--- app/controllers/series_controller.py
+++ app/controllers/series_controller.py
@@ -97,37 +97,22 @@ def find_by_genre(genre_name, title_name = None):
     ).select_from(SeriesModel).join(series_genders).join(GendersModel).filter(
     series_genders.gender_id == genre_id, SeriesModel.name.ilike(f"%{title_name}%")).all()
 
-###############SERIALIZAÇÃO COMPLETA
-    # return jsonify([
-    #     {
-    #        "id": serie.id,
-    #        "name": serie.name,
-    #        "image": serie.image,
-    #        "description": serie.description,
-    #        "seasons": serie.seasons,
-    #        "trailer": serie.trailer,
-    #        "created_at": serie.created_at,
-    #        "views": serie.views,
-    #        "dubbed": serie.dubbed,
-    #        "subtitle": serie.subtitle,
-    #        "classification": serie.classification,
-    #        "released_date": serie.released_date
-    #     } for serie in series  ]), 200
-
-    return jsonify([
-        {
+    return [{
+           "id": serie.id,
            "name": serie.name,
            "image": serie.image,
            "description": serie.description,
            "seasons": serie.seasons,
-           "episodes": [
-               {
-                   "season": episode.season, 
-                    "link": episode.link, 
-                    "episode": episode.episode
-               } for episode in get_episodes(serie.id)
-           ]
-        } for serie in series  ]), 200
+           "trailer": serie.trailer,
+           "created_at": serie.created_at,
+           "views": serie.views,
+           "dubbed": serie.dubbed,
+           "subtitle": serie.subtitle,
+           "classification": serie.classification,
+           "released_date": serie.released_date
+        } for serie in series  ]
+
+    
 
 
 def get_episodes(id: int):
