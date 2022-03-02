38e523bb83c51ed6f259ece6a61da9aa51899be0
IqueMoraes <iquedemoraes@hotmail.com>
2022-03-02 14:58:41 -0300

    associated functions to bp_movies.get

--- app/routes/movies_routes.py
+++ app/routes/movies_routes.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
-from app.controllers.movies_controller import get_movies_by_name, get_movies_by_genre
+from app.controllers.movies_controller import get_movies
 
 bp_movies = Blueprint("movies", __name__, url_prefix="/movies")
 
 
-bp_movies.get("/<title>")(get_movies_by_name)
-bp_movies.get("/<genre>")(get_movies_by_genre)
+bp_movies.get("")(get_movies)
 
