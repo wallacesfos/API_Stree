6a4a1be378f727c2255381117b960fadbfd2e584
Ricardo Oliveira <ricardonegocios700@gmail.com>
2022-03-01 21:04:58 -0300

    feat: working, pause to check migrate

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,9 +3,11 @@ from app.routes.users_route import bp_users
 from app.routes.profiles_routes import bp_profile
 from app.routes.series_route import bp_series
 from app.routes.episodes_route import bp_episodes
+from app.routes.genders_routes import bp_genders
 
 def init_app(app: Flask):
     app.register_blueprint(bp_users)
     app.register_blueprint(bp_series)
     app.register_blueprint(bp_profile)
     app.register_blueprint(bp_episodes)
+    app.register_blueprint(bp_genders)
