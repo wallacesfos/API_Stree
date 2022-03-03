51f725f6988f8df3cf37fe52be4267baec3077f6
IqueMoraes <iquedemoraes@hotmail.com>
2022-03-03 20:10:40 -0300

    fix associated controller to routes

--- app/routes/movies_route.py
+++ app/routes/movies_route.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
 
-from app.controllers.movies_controller import get_series_by_genre
+from app.controllers.movies_controller import (
+    get_series_by_genre,
+    post_favorite,
+    remove_favorite
+)
 
 bp_movies = Blueprint("movies", __name__, url_prefix="/movies")
 
 # bp_series.post("")(create_serie)
 bp_movies.get("/<int:profile_id>")(get_series_by_genre)
-
+bp_movies.post("/favorites")(post_favorite)
+bp_movies.delete("/favorites")(remove_favorite)
