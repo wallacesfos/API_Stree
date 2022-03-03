43b2a91d088e039e4ae85a2c5266451e767cb335
IqueMoraes <iquedemoraes@hotmail.com>
2022-03-03 19:34:49 -0300

    feat search movies by title

--- app/controllers/movies_controller.py
+++ app/controllers/movies_controller.py
@@ -8,3 +8,13 @@ from app.exc import EmptyListError
 from app.utils import find_by_genre
 
 
+@jwt_required()
+def get_movies_by_name(profile_id: int, title: str):
+
+    movies = MoviesModel.query.filter_by(MoviesModel.name.ilike(f"%{title}%")).all()
+
+    profile = ProfileModel.query.filter(id=profile_id).one_or_404("Profile not found")
+    if profile.kids:
+        return jsonify([movie for movie in movies if movie.classification <= 13]), HTTPStatus.OK
+
+    return jsonify(movies), HTTPStatus.OK
