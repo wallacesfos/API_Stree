0cf24cbb2189abe2f07877960953e4dc72a40d66
IqueMoraes <iquedemoraes@hotmail.com>
2022-03-03 15:49:27 -0300

    feat delete movie if user admin

--- app/controllers/movies_controller.py
+++ app/controllers/movies_controller.py
@@ -1,10 +1,28 @@
-from flask import jsonify, request
-from flask_jwt_extended import jwt_required
+from flask import jsonify, request, current_app
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from http import HTTPStatus
 
 from app.models.profile_model import ProfileModel
 from app.models.movies_model import MoviesModel
-from app.exc import EmptyListError
+from app.models.user_model import UserModel
+from app.exc import EmptyListError, PermissionError
 from app.utils import find_by_genre
 
+
+@jwt_required()
+def delete_movie(id: int):
+    try:
+        user: UserModel = get_jwt_identity()
+        if not user['administer']:
+            raise PermissionError
+
+        movie = MoviesModel.query.filter(id=id).one_or_404(discription="Movie not found")
+
+        current_app.db.session.delete(movie)
+        current_app.db.session.commit()
+
+        return {}, HTTPStatus.NO_CONTENT
+    
+    except PermissionError:
+        return {"error": "Admins only"}, HTTPStatus.BAD_REQUEST
 
--- app/routes/movies_route.py
+++ app/routes/movies_route.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
 
-from app.controllers.movies_controller import get_series_by_genre
+from app.controllers.movies_controller import (
+    get_series_by_genre,
+    delete_movie
+)
 
 bp_movies = Blueprint("movies", __name__, url_prefix="/movies")
 
 # bp_series.post("")(create_serie)
 bp_movies.get("/<int:profile_id>")(get_series_by_genre)
+bp_movies.delete("/<int:id>")(delete_movie)
+
 
