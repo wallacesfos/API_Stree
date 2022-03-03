8275c635978973652f34ca9d87756d969636c78e
IqueMoraes <iquedemoraes@hotmail.com>
2022-03-03 19:46:07 -0300

    feat adds movies to favorites list of profile

--- app/controllers/movies_controller.py
+++ app/controllers/movies_controller.py
@@ -1,10 +1,31 @@
-from flask import jsonify, request
-from flask_jwt_extended import jwt_required
+from flask import jsonify, request, current_app
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from http import HTTPStatus
 
 from app.models.profile_model import ProfileModel
 from app.models.movies_model import MoviesModel
+from app.models.user_model import UserModel
 from app.exc import EmptyListError
 from app.utils import find_by_genre
 
 
+
+@jwt_required()
+def post_favorite():
+    try:
+        data = request.get_json()
+        user = UserModel.query.filter_by(id=get_jwt_identity()["id"]).first_or_404("User not found")
+        profile = ProfileModel.query.filter_by(id=data["profile_id"]).first_or_404("Profile not found")
+        
+        if not profile in user.profiles:
+            return jsonify({"error": "Invalid profile for user"}), HTTPStatus.CONFLICT
+        
+        serie = MoviesModel.query.filter_by(id=data["movie_id"]).first_or_404("Movie not found")
+        profile.series.append(serie)
+        current_app.db.session.add(profile)
+        current_app.db.session.commit()
+
+    except Exception as e:
+        return {"error": e.description}, HTTPStatus.NOT_FOUND
+    
+    return jsonify({}), HTTPStatus.NO_CONTENT
