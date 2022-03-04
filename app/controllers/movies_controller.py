29a7a4980aea359901d8598b9370450ee8b5f6fd
IqueMoraes <83541379+IqueMoraes@users.noreply.github.com>
2022-03-04 10:56:12 -0300

    Update movies_controller.py

--- app/controllers/movies_controller.py
+++ app/controllers/movies_controller.py
@@ -10,11 +10,20 @@ from app.utils import find_by_genre
 
 @jwt_required()
 def get_movies_by_name(profile_id: int, title: str):
+    try:
+        movies = MoviesModel.query.filter_by(MoviesModel.name.ilike(f"%{title}%")).all()
 
-    movies = MoviesModel.query.filter_by(MoviesModel.name.ilike(f"%{title}%")).all()
+        profile = ProfileModel.query.filter(id=profile_id).first()
 
-    profile = ProfileModel.query.filter(id=profile_id).one_or_404("Profile not found")
-    if profile.kids:
-        return jsonify([movie for movie in movies if movie.classification <= 13]), HTTPStatus.OK
+        if not profile:
+            return {"error": "Profile not found"}, HTTPStatus.NOT_FOUND
 
-    return jsonify(movies), HTTPStatus.OK
+        if profile.kids:
+            return jsonify([movie for movie in movies if movie.classification <= 13]), HTTPStatus.OK
+
+        if not movies:
+            raise EmptyListError
+
+        return jsonify(movies), HTTPStatus.OK
+    except EmptyListError as e:
+        return {"error": e.description}, e.code
