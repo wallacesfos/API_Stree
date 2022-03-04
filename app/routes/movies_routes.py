c4a7b621d8d1ba8017c14a6addbf2745183dcb35
Smilly Delmondes <smilly.3d@gmail.com>
2022-03-04 11:22:35 -0300

    fix: most seen

--- app/routes/movies_routes.py
+++ app/routes/movies_routes.py
@@ -2,11 +2,15 @@ from flask import Blueprint
 
 from app.controllers.movies_controller import (
     create_movie,
-    delete_movie
+    delete_movie,
+    get_most_recent_movies,
+    get_most_seen_movies
 )
 
 bp_movies = Blueprint("movies", __name__, url_prefix="/movies")
 
-
+bp_movies.get("/most_seen")(get_most_seen_movies)
+bp_movies.get("/most_recent")(get_most_recent_movies)
 bp_movies.post("")(create_movie)
 bp_movies.delete("/<int:id>")(delete_movie)
+
