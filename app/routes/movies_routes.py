87c472d6b50ab05e4144b6ab71123b4f4989b36e
wallacesfos <wallacesfos@hotmail.com>
2022-03-09 10:58:08 -0300

    Fix: Movies routes get movies by name

--- app/routes/movies_routes.py
+++ app/routes/movies_routes.py
@@ -19,6 +19,7 @@ from app.controllers.movies_controller import (
 
 bp_movies = Blueprint("movies", __name__, url_prefix="/movies")
 
+bp_movies.get("/")(get_movies_by_name)
 bp_movies.get("/most_seen")(get_most_seen_movies)
 bp_movies.get("/most_recent")(get_most_recent_movies)
 bp_movies.post("")(create_movie)
@@ -26,7 +27,6 @@ bp_movies.delete("/<int:id>")(delete_movie)
 bp_movies.patch("/<int:id>")(update_movie)
 bp_movies.delete('/gender')(remove_from_gender)
 bp_movies.post('/gender')(add_to_gender)
-bp_movies.get("/")(get_movies_by_name)
 bp_movies.get("/<int:id>")(get_movie_by_id)
 bp_movies.get('/<genre_name>')(get_movies_by_genre)
 bp_movies.post("/favorite")(post_favorite)
