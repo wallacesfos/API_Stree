c91e757b8127029b4becbfeab165e07dd9affc63
IqueMoraes <iquedemoraes@hotmail.com>
2022-03-08 21:59:43 -0300

    created get to all movies

--- app/routes/movies_routes.py
+++ app/routes/movies_routes.py
@@ -10,11 +10,14 @@ from app.controllers.movies_controller import (
     update_movie,
     get_most_recent_movies,
     get_most_seen_movies,
-    remove_from_gender
+    remove_from_gender,
+    get_movies_by_name,
+    get_movies
 )
 
 bp_movies = Blueprint("movies", __name__, url_prefix="/movies")
 
+bp_movies.get("")(get_movies)
 bp_movies.get("/most_seen")(get_most_seen_movies)
 bp_movies.get("/most_recent")(get_most_recent_movies)
 bp_movies.post("")(create_movie)
@@ -22,4 +25,4 @@ bp_movies.delete("/<int:id>")(delete_movie)
 bp_movies.patch("/<int:id>")(update_movie)
 bp_movies.delete('/gender')(remove_from_gender)
 bp_movies.post('/gender')(add_to_gender)
-
+bp_movies.get("/")(get_movies_by_name)
